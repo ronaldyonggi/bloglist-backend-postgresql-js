49013241cb313ac5b9565020b62b2372b325bb8f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-24 15:26:46 +0800

    13.4 created Blog model

--- src/index.js
+++ src/index.js
@@ -15,4 +15,32 @@ const main = async () => {
   }
 }
 
-main()
+class Blog extends Model {}
+Model.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+Blog.sync()

c15ddf001b87f4083989349c99cdc606c5b4ea6c
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-25 14:13:17 +0800

    13.5 index is now only for starting app

--- src/index.js
+++ src/index.js
@@ -1,65 +1,18 @@
-const config = require('./utils/config')
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
-const sequelize = new Sequelize(config.DATABASE_URL)
+const {  connectToDatabase } = require('./utils/db')
+const blogsRouter = require('./controllers/blogs')
+const { PORT } = require('./utils/config')
 
-class Blog extends Model {}
-Model.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
-Blog.sync()
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', async (_req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+}
 
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const newBlog = await Blog.create(req.body)
-    return res.json(newBlog)
-  } catch(error) {
-    return res.status(400).json({ error })
-  }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const blogToBeDeleted = await Blog.findByPk(req.params.id)
-  if (blogToBeDeleted) {
-    await blogToBeDeleted.destroy()
-    return res.status(204).end()
-  } else {
-    res.status(400).end()
-  }
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()

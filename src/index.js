a8dd46242fbed783be22e5b2c5d00973f6f00324
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-30 18:35:11 +0700

    13.7 app now uses errorHandler middleware

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ const app = express()
 const {  connectToDatabase } = require('./utils/db')
 const blogsRouter = require('./controllers/blogs')
 const { PORT } = require('./utils/config')
+const errorHandler = require('./utils/middleware')
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use(errorHandler)
 
 const start = async () => {
   await connectToDatabase()

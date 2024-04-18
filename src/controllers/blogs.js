89e21101e2a620b646127de1728555cb3ee7ed65
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-04-18 15:53:57 +0700

    Prettier formatting fix

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,11 +1,11 @@
-const blogsRouter = require('express').Router()
-const { Blog } = require('../models')
+const blogsRouter = require('express').Router();
+const { Blog, User } = require('../models');
 
 // Middleware for finding a specific blog
 const blogFinder = async (req, res, next) => {
-  req.blog = await Blog.findByPk(req.params.id)
-  next()
-}
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
 
 // GET all blogs
 blogsRouter.get('/', async (req, res) => {
@@ -21,60 +21,59 @@ blogsRouter.get('/', async (req, res) => {
 // GET a specific blog
 blogsRouter.get('/:id', blogFinder, async (req, res) => {
   if (req.blog) {
-    res.json(req.blog)
+    res.json(req.blog);
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
 // CREATE a blog
 blogsRouter.post('/', async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
   if (!user) {
-    return res.status(400).json({ error: 'User not found!' })
+    return res.status(400).json({ error: 'User not found!' });
   }
 
   try {
     const newBlog = {
       ...req.body,
-      userId: user.id
-    }
-    const blog = await Blog.create(newBlog)
-    return res.json(blog)
+      userId: user.id,
+    };
+    const blog = await Blog.create(newBlog);
+    return res.json(blog);
   } catch (error) {
-    return res.status(400).json({ error })
+    return res.status(400).json({ error });
   }
-})
+});
 
 blogsRouter.delete('/:id', blogFinder, async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
   if (!user) {
-    return res.status(400).json({ error: 'User not found!' })
+    return res.status(400).json({ error: 'User not found!' });
   }
 
   if (!req.blog) {
-    return res.status(400).json({ error: 'blog not found!'})
+    return res.status(400).json({ error: 'blog not found!' });
   }
 
   if (!(user.id === req.blog.userId)) {
-    return res.status(401).json({ error: 'Unauthorized to delete blog!'})
+    return res.status(401).json({ error: 'Unauthorized to delete blog!' });
   }
 
-  await req.blog.destroy()
-  return res.status(204).end()
-
-})
+  await req.blog.destroy();
+  return res.status(204).end();
+});
 
 blogsRouter.put('/:id', blogFinder, async (req, res) => {
   if (req.blog) {
-    req.blog.likes = req.body.likes
-    await req.blog.save()
-    res.json(req.blog)
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;

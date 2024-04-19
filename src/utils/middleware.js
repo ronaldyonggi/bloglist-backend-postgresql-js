24258c9d8a0741e93696ec92ffe4f65e84610274
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-04-19 19:57:26 +0700

    Prettier formatting fix

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,48 +1,49 @@
-const jwt = require('jsonwebtoken')
-const { User } = require('../models')
-const { SECRET } = require('../utils/config')
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+const { SECRET } = require('../utils/config');
 
 // If request header Authorization contains a token, sets req.token to be that token
 const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
+  const authorization = req.get('authorization');
   if (authorization && authorization.startsWith('Bearer ')) {
-    req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
   }
 
-  next()
-}
+  next();
+};
 
 const userExtractor = async (req, res, next) => {
   if (req.decodedToken) {
-    const user = await User.findByPk(req.decodedToken.id)
+    const user = await User.findByPk(req.decodedToken.id);
     if (user) {
-      req.user = user
+      req.user = user;
     }
   }
 
-  next()
-}
+  next();
+};
 
 // Error handler middleware
 const errorHandler = (error, req, res, next) => {
-  console.log('Error name: ' + error.name)
-  console.log('Error message: ' + error.message)
+  console.log('Error name: ' + error.name);
+  console.log('Error message: ' + error.message);
 
   switch (error.name) {
     case 'SequelizeValidationError':
-      return res.status(400).json({ error: 'Username needs to use email format!' })
+      return res
+        .status(400)
+        .json({ error: 'Username needs to use email format!' });
     case 'JsonWebTokenError':
-      return res.status(401).json({ error: 'Token missing or invalid!'})
+      return res.status(401).json({ error: 'Token missing or invalid!' });
     case 'TokenExpiredError':
-      return res.status(401).json({ error: 'Token expired'})
+      return res.status(401).json({ error: 'Token expired' });
     default:
-      return res.status(400).json({ error })
+      return res.status(400).json({ error });
   }
-
-}
+};
 
 module.exports = {
   tokenExtractor,
   userExtractor,
-  errorHandler
-}
+  errorHandler,
+};

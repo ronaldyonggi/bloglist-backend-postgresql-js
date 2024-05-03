cf6987597c19479b9d5e8c3287f3ef152873629d
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-03 10:43:09 +0800

    refactor migration code to have separate section for rollback

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,20 +3,27 @@ const config = require('./config');
 const { Umzug, SequelizeStorage } = require('umzug');
 const sequelize = new Sequelize(config.DATABASE_URL);
 
-const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: 'migrations/*.js',
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  });
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
 
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
   const migrations = await migrator.up();
   console.log('Migrations up to date', {
     files: migrations.map((mig) => mig.name),
   });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
 };
 
 const connectToDatabase = async () => {
@@ -34,4 +41,5 @@ const connectToDatabase = async () => {
 module.exports = {
   connectToDatabase,
   sequelize,
+  rollbackMigration
 };

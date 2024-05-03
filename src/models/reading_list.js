293d44eecfb09c8396ebe3309d2d1226a02a4ab1
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-03 10:44:59 +0800

    13.19 define reading list model schema

---
+++ src/models/reading_list.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const { sequelize } = require('../utils/db');
+
+class ReadingList extends Model {}
+
+ReadingList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'blogs',
+        key: 'id',
+      },
+    },
+    read: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: 'readingList',
+  }
+);
+
+module.exports = ReadingList;

a0df0b0a8f159c753b3834974312f648e9b3cbf3
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-03 10:45:25 +0800

    13.19 define relationship between users and blogs in reading list

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,12 @@
 const Blog = require('./blog');
+const ReadingList = require('./reading_list');
 const User = require('./user');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, { through: ReadingList, as: 'marked_blogs' });
+Blog.belongsToMany(User, { through: ReadingList, as: 'users_marked' });
 
 module.exports = {
   Blog,

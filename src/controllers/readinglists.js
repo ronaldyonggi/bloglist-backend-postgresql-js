5bf569e722e8c4ecff420cb17361901686593c76
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-05 11:28:03 +0800

    define a route to GET all blogs

--- src/controllers/readinglists.js
+++ src/controllers/readinglists.js
@@ -1,6 +1,13 @@
 const readinglistsRouter = require('express').Router();
 const { ReadingList } = require('../models');
 
+// GET all reading lists
+readinglistsRouter.get('/', async (req, res) => {
+  const readingLists = await ReadingList.findAll();
+  res.json(readingLists);
+});
+
+// CREATE a reading list, need a logged in user and a blog
 readinglistsRouter.post('/', async (req, res) => {
   const user = req.user;
 

de03f423ff62a729286464204a381549686d4ff6
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-05 21:39:21 +0800

    handle duplicate when creating new entry for reading list

--- src/controllers/readinglists.js
+++ src/controllers/readinglists.js
@@ -10,12 +10,33 @@ readinglistsRouter.get('/', async (req, res) => {
 // CREATE a reading list, need a logged in user and a blog
 readinglistsRouter.post('/', async (req, res) => {
   const user = req.user;
-
   if (!user) {
     return res.status(400).json({ error: 'User not found!' });
   }
 
   const userId = user.id;
+  const { blogId } = req.body;
+
+  const alreadyExist = await ReadingList.findOne({
+    where: {
+      userId: userId,
+      blogId: blogId,
+    },
+  });
+
+  // Handle case if the user already added the blog to the user list. Prevents duplicate record
+  if (alreadyExist) {
+    return res
+      .status(409)
+      .json({ error: 'blog already added to reading list by this user!' });
+  }
+
+  const successfulCreatedReadingList = await ReadingList.create({
+    blogId,
+    userId,
+  });
+  return res.json(successfulCreatedReadingList);
+});
 
 // PUT a reading list, changing its "read" attribute from false to true, or vice versa
 readinglistsRouter.put('/:id', async (req, res) => {

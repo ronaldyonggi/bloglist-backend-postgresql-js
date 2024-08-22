85a917d08a2656ef3d21dc068471c57ce713313a
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-22 21:33:43 +0800

    13.24 app uses logout router

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const { connectToDatabase } = require('./utils/db');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const logoutRouter = require('./controllers/logout');
 const { PORT } = require('./utils/config');
 const authorsRouter = require('./controllers/authors');
 const readinglistsRouter = require('./controllers/readinglists');
@@ -21,6 +22,7 @@ app.use('/api/readinglists', middleware.userExtractor, readinglistsRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/authors', authorsRouter);
+app.use('/api/logout', middleware.userExtractor, logoutRouter);
 app.use(middleware.errorHandler);
 
 const start = async () => {
